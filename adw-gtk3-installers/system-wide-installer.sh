bdb3b6fc2293efc137149defd53dc326cc0a5cba
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 14:23:08 -0500

    Update system-wide-installer.sh

--- adw-gtk3-installers/system-wide-installer.sh
+++ adw-gtk3-installers/system-wide-installer.sh
@@ -2,8 +2,8 @@
 sudo echo "Making directories"
 sudo mkdir -p /usr/local/share
 sudo mkdir -p /usr/local/share/themes
-sudo rm -f /usr/local/share/themes/adw-gtk3
-sudo rm -f /usr/local/share/themes/adw-gtk3-dark
+sudo rm -rf /usr/local/share/themes/adw-gtk3
+sudo rm -rf /usr/local/share/themes/adw-gtk3-dark
 sudo echo "Downloading theme archive"
 wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz
 echo "Extracting theme archive to /usr/local/share/themes"
