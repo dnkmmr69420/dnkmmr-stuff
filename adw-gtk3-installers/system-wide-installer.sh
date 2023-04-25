208a9d775af7d1fb92c17fee59aa99ec649c067b
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 14:20:31 -0500

    updated from v4.3 to v4.5

--- adw-gtk3-installers/system-wide-installer.sh
+++ adw-gtk3-installers/system-wide-installer.sh
@@ -3,7 +3,7 @@ sudo echo "Making directories"
 sudo mkdir -p /usr/local/share
 sudo mkdir -p /usr/local/share/themes
 sudo echo "Downloading theme archive"
-wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.3/adw-gtk3v4-3.tar.xz
+wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz
 echo "Extracting theme archive to /usr/local/share/themes"
 sudo tar -xf adw-gtk3v4-3.tar.xz -C /usr/local/share/themes
 sudo echo "theme should now be installed"
