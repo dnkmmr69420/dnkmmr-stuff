6bcbe357d9a261a48125c1e68861b8ecc2270774
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-28 14:24:40 -0500

    fixed the name of the tar it is extracting

--- adw-gtk3-installers/system-wide-installer.sh
+++ adw-gtk3-installers/system-wide-installer.sh
@@ -7,5 +7,5 @@ sudo rm -rf /usr/local/share/themes/adw-gtk3-dark
 sudo echo "Downloading theme archive"
 wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz
 echo "Extracting theme archive to /usr/local/share/themes"
-sudo tar -xf adw-gtk3v4-3.tar.xz -C /usr/local/share/themes
+sudo tar -xf adw-gtk3v4-5.tar.xz -C /usr/local/share/themes
 sudo echo "theme should now be installed"
