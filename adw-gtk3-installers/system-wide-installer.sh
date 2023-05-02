0c90d51aff69a4a3fb2f6ac59d1a012c828f71e8
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-02 07:59:44 -0500

    updated from v4-5 to v4-6

--- adw-gtk3-installers/system-wide-installer.sh
+++ adw-gtk3-installers/system-wide-installer.sh
@@ -5,7 +5,8 @@ sudo mkdir -p /usr/local/share/themes
 sudo rm -rf /usr/local/share/themes/adw-gtk3
 sudo rm -rf /usr/local/share/themes/adw-gtk3-dark
 sudo echo "Downloading theme archive"
-wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.5/adw-gtk3v4-5.tar.xz
+wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.6/adw-gtk3v4-6.tar.xz
 echo "Extracting theme archive to /usr/local/share/themes"
-sudo tar -xf adw-gtk3v4-5.tar.xz -C /usr/local/share/themes
+sudo tar -xf adw-gtk3v4-6.tar.xz -C /usr/local/share/themes
+rm ./adw-gtk3v4-6.tar.xz
 sudo echo "theme should now be installed"
