62b8db70f23049310553293530e19e3bbbe00885
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-26 08:57:11 -0500

    Update setup.sh

--- NixOS-shebang-compatability/setup.sh
+++ NixOS-shebang-compatability/setup.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+if ! command -v nixos-rebuild &> /dev/null
+then
+    echo "This script has detected that you aren't running on NixOS. This script has stopped here to prevent damage to your system."
+    exit 95
+fi
+
 sudo echo "Creating symlinks..."
 sudo rm -rf /bin
 sudo ln -sf /usr/bin /bin
