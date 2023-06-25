d524eb809cd0875f7a03eb4497da9eb69ba4a3ff
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-25 10:32:30 -0500

    Update symlink.nix

--- symlink-for-nix/symlink.nix
+++ symlink-for-nix/symlink.nix
@@ -1,9 +1,13 @@
 { ... }:
 
 {
-  # create symlink to Bash executable
-  builtins.symlink "/bin/bash" "/run/current-system/sw/bin/bash";
-
-  # create symlink to default Nix profile
-  builtins.symlink "/nix/nix-profile" "/nix/var/nix/profiles/default";
+  systemd.services.create-symlinks = {
+    script = ''
+      mv -f /bin/sh /usr/bin/sh
+      ln -sf /run/current-system/sw/bin/bash /bin/bash
+      ln -sf /nix/var/nix/profiles/default /nix/nix-profile
+    '';
+    serviceConfig.Type = "oneshot";
+    wantedBy = [ "multi-user.target" ];
+  };
 }
