771ab43d7ed27e3038b94c68432b319e03755fd5
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-25 10:41:19 -0500

    Update symlink.nix

--- symlink-for-nix/symlink.nix
+++ symlink-for-nix/symlink.nix
@@ -3,7 +3,6 @@
 {
   systemd.services.create-symlinks = {
     script = ''
-      mv -f /bin/sh /usr/bin/sh
       ln -sf /run/current-system/sw/bin/bash /bin/bash
       ln -sf /nix/var/nix/profiles/default /nix/nix-profile
     '';
